5f60a065ebcccfee0d433d8b48f9d175fbb3a8f5
Muhammad Rizo <abduganeoff@gmail.com>
2021-06-07 16:12:50 +0200

    Streams are listed in StreamList component

--- src/components/streamy/StreamList.js
+++ src/components/streamy/StreamList.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography';
 
-function StreamList() {
-	return <div >StreamList</div>;
+class StreamList extends Component {
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
+
+	renderList() {
+		return this.props.streams.map((stream) => {
+			return (
+				<React.Fragment key={stream.id}>
+					<ListItem>
+						<ListItemAvatar>
+							<Avatar>
+								<OndemandVideoIcon />
+							</Avatar>
+						</ListItemAvatar>
+						<ListItemText
+							primary={stream.title}
+							secondary={stream.description}
+						/>
+					</ListItem>
+					<Divider />
+				</React.Fragment>
+			);
+		});
+	}
+
+	render() {
+		return (
+			<List>
+				<Typography variant="h5" style={{ margin: ' 0 0 5px 15px' }}>
+					Streams
+				</Typography>
+				{this.props.streams.length ? <Divider /> : null}
+				{this.renderList()}
+			</List>
+		);
+	}
 }
 
-export default StreamList;
+const mapStateToProps = (state) => {
+	return { streams: Object.values(state.streams) };
+};
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
