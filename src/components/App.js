e2b0544785f9409c32f019dc16716eaaa6dfc2a6
Muhammad Rizo <abduganeoff@gmail.com>
2021-06-07 16:18:59 +0200

    history of browser is implemented manually and passed into Router instead Browser Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 import StreamList from './streamy/StreamList';
 import StreamCreate from './streamy/StreamCreate';
 import StreamEdit from './streamy/StreamEdit';
 import StreamDelete from './streamy/StreamDelete';
 import StreamShow from './streamy/StreamShow';
 import Header from './Header';
+import history from '../history';
 
 function App() {
 	return (
 		<div>
-			<BrowserRouter>
+			<Router history={history}>
 				<Header />
 				<div style={{ marginTop: '90px' }}>
 					<Route path="/" exact component={StreamList} />
@@ -23,7 +24,7 @@ function App() {
 					/>
 					<Route path="/streams/show" exact component={StreamShow} />
 				</div>
-			</BrowserRouter>
+			</Router>
 		</div>
 	);
 }
